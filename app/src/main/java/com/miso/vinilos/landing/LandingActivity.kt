c311398e363ed75b2e13669e23152e68b1965c04
Willian Andres Piarpuzan <w.piarpuzan@uniandes.edu.co>
2024-11-09 10:19:57 -0500

    avance implementacion crear album

--- app/src/main/java/com/miso/vinilos/landing/LandingActivity.kt
+++ app/src/main/java/com/miso/vinilos/landing/LandingActivity.kt
@@ -1,12 +1,16 @@
 package com.miso.vinilos.landing
 
 import android.content.Intent
+import android.content.res.ColorStateList
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import com.miso.vinilos.R
 import com.miso.vinilos.main.MainActivity
 import com.miso.vinilos.ui.listactivities.AlbumListActivity
@@ -17,12 +21,20 @@ class LandingActivity : AppCompatActivity() {
     private lateinit var actButton: FloatingActionButton
     private lateinit var backToMainBtn: ImageButton
 
+    //Configuración expandir botón crear album, asociar track a album
+    private var isFabExpanded = false
+    // Referencias a los botones
+    private lateinit var fabLeft: FloatingActionButton
+    private lateinit var fabRight: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.landing)
 
         actButton = findViewById(R.id.act)
         backToMainBtn = findViewById(R.id.btnBackToMain)
+        fabLeft = findViewById(R.id.fab_left)
+        fabRight = findViewById(R.id.fab_right)
 
         validateAdmin()
         bindButtonActions()
@@ -33,7 +45,33 @@ class LandingActivity : AppCompatActivity() {
         if (isAdmin) {
             actButton.show()
             actButton.setOnClickListener {
+                val layoutParams = actButton.layoutParams as ConstraintLayout.LayoutParams
                 // Acción para admin
+                if (isFabExpanded) {
+                    fabLeft.visibility = View.GONE
+                    fabRight.visibility = View.GONE
+                    actButton.setImageResource(R.drawable.ic_add)
+                    actButton.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(this, R.color.backgroundPrimary))
+
+                    // Restablecer las restricciones para alinear a la derecha
+                    layoutParams.endToEnd = ConstraintLayout.LayoutParams.PARENT_ID
+                    layoutParams.startToStart = ConstraintLayout.LayoutParams.UNSET // Remover restricción de inicio si existe
+                    actButton.layoutParams = layoutParams
+
+                    isFabExpanded = false
+                } else {
+                    fabLeft.visibility = View.VISIBLE
+                    fabRight.visibility = View.VISIBLE
+                    actButton.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(this, R.color.black))
+                    actButton.setImageResource(R.drawable.ic_add)
+
+                    // Establecer las restricciones para centrar
+                    layoutParams.startToStart = ConstraintLayout.LayoutParams.PARENT_ID
+                    layoutParams.endToEnd = ConstraintLayout.LayoutParams.PARENT_ID
+                    actButton.layoutParams = layoutParams
+                    
+                    isFabExpanded = true
+                }
             }
         } else {
             actButton.hide()
