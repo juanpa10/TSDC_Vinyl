71a53151f2abb34df3d15e2efbb5baf2661fe742
Willian Andres Piarpuzan <w.piarpuzan@uniandes.edu.co>
2024-11-30 16:53:17 -0500

    Se configura prueba espresso para crear album

--- app/src/androidTest/java/com/miso/vinilos/VinilosCreateInstrumentalTest.kt
+++ app/src/androidTest/java/com/miso/vinilos/VinilosCreateInstrumentalTest.kt
@@ -1,6 +1,7 @@
 package com.miso.vinilos
 
 
+import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
@@ -27,6 +28,10 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.instanceOf
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.core.IsNot.not
 
 @RunWith(AndroidJUnit4::class)
 class VinilosCreateInstrumentalTest {
@@ -52,13 +57,22 @@ class VinilosCreateInstrumentalTest {
     fun testBtnCreateAlbum() {
         onView(withId(R.id.fab_left)).perform(click())
         Intents.intended(IntentMatchers.hasComponent(CreateAlbumActivity::class.java.name))
-        onView(withId(R.id.et_nombre)).perform(typeText("Muevense"), closeSoftKeyboard())
-        onView(withId(R.id.et_caratula)).perform(typeText("https://akamai.sscdn.co/uploadfile/letras/albuns/e/c/7/e/2206181714392266.jpg"), closeSoftKeyboard())
+        onView(withId(R.id.et_nombre)).perform(typeText("Valio la Pena"), closeSoftKeyboard())
+        onView(withId(R.id.et_caratula)).perform(typeText("https://www.lamusica.com.co/cdn/shop/products/R-7014851-1431711913-3333_jpeg.jpg"), closeSoftKeyboard())
         onView(withId(R.id.et_fecha_lanzamiento)).perform(typeText("Marc Anthony"), closeSoftKeyboard())
-        onView(withId(R.id.et_descripcion)).perform(typeText("Marc Anthony"), closeSoftKeyboard())
-        onView(withId(R.id.et_genero)).perform(typeText("Marc Anthony"), closeSoftKeyboard())
-        onView(withId(R.id.et_etiqueta)).perform(typeText("Marc Anthony"), closeSoftKeyboard())
-        //onView(withText("Álbum creado con éxito")).inRoot(withDecorView(not('is'(activityRule.activity.window.decorView)))).check(matches(isDisplayed()))
+        onView(withId(R.id.et_descripcion)).perform(typeText("Álbum de Salsa de Marc Anthony con 8 éxitos musicales, se encuentran canción como Ahora quien, valió la pena, entre otras"), closeSoftKeyboard())
+
+        onView(withId(R.id.et_genero)).perform(click())
+        onData(allOf(`is`(instanceOf(String::class.java)), `is`("Salsa"))).perform(click())
+
+        onView(withId(R.id.et_etiqueta)).perform(click())
+        onData(allOf(`is`(instanceOf(String::class.java)), `is`("Sony Music"))).perform(click())
+
+        onView(withId(R.id.btnUsuario2)).perform(click())
+
+        activityRule.scenario.onActivity { activity ->
+            onView(withText("Álbum creado exitosamente")).inRoot(withDecorView(not(`is`(activity.window.decorView)))).check(matches(isDisplayed()))
+        }
     }
 
     @Test
